ab4d5f3ef3a4e135ac998a3b68a2fba61023913b
vedantbhardwaj15 <91656743+vedantbhardwaj15@users.noreply.github.com>
2021-09-30 12:35:10 +0530

    Add files via upload

---
+++ calendarproblem.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int leapdays,firstday,yr;
+    long int normaldays,totaldays;
+
+    printf("Enter year:");
+    scanf("%d",&yr);
+    
+    normaldays=(yr-1)*365;
+    leapdays=(yr-1)/4-(yr-1)/100+(yr-1)/400;
+    totaldays=normaldays+leapdays;
+    
+    firstday=totaldays%7;
+
+    
+    switch(firstday)
+    {
+        case 0:
+            printf("Monday");
+        break;
+        case 1:
+            printf("Tuesday");
+        break;
+        case 2:
+            printf("Wednesday");
+        break;
+        case 3:
+            printf("Thursday");
+        break;
+        case 4:
+            printf("Friday");
+        break;
+        case 5:
+            printf("Saturday");
+        break;
+        case 6:
+            printf("Sunday");
+        break;
+        default:
+            printf("Error");
+    }        
+    return 0;
+}
